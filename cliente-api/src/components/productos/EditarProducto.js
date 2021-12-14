d45342bdb7198fa2181dd88240cb1a37a04ebc0f
Diego <drbv27@gmail.com>
2021-12-13 23:09:36 -0500

    Obtaining the product to edit

--- cliente-api/src/components/productos/EditarProducto.js
+++ cliente-api/src/components/productos/EditarProducto.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import Swal from "sweetalert2";
+import { useParams } from "react-router-dom";
+import clienteAxios from "../../config/axios";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 function EditarProductos() {
+  //obtener el id del producto
+  const { id } = useParams();
+
+  //producto = state y funcion para actualizar
+  const [producto, guardarProducto] = useState({
+    nombre: "",
+    precio: "",
+    imagen: "",
+  });
+
+  //useEffect cuando el componente carga
+  useEffect(() => {
+    //consultar la api para traer el producto a editar
+    const consultarAPI = async () => {
+      const productoConsulta = await clienteAxios.get(`/productos/${id}`);
+      console.log(productoConsulta);
+      guardarProducto(productoConsulta.data);
+    };
+    consultarAPI();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <h2>Editar Productos</h2>
