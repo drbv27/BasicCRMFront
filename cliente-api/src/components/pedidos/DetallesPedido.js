db69c6d6afe7aec44f2ebd8218847e5665b659c2
Diego <drbv27@gmail.com>
2021-12-18 18:51:47 -0500

    Deleting pedidos

--- cliente-api/src/components/pedidos/DetallesPedido.js
+++ cliente-api/src/components/pedidos/DetallesPedido.js
@@ -1,7 +1,28 @@
 import React from "react";
+import clienteAxios from "../../config/axios";
+import Swal from "sweetalert2";
 
 function DetallesPedido({ pedido }) {
   const { cliente } = pedido;
+
+  const eliminarPedido = (id) => {
+    Swal.fire({
+      title: "¿Estas seguro?",
+      text: "Luego no hay vuelta atrás",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, elimínalo!",
+    }).then((result) => {
+      if (result.value) {
+        // llamado a axios
+        clienteAxios.delete(`/pedidos/${id}`).then((res) => {
+          Swal.fire("Eliminado!", "El pedido se ha eliminado.", "success");
+        });
+      }
+    });
+  };
 
   return (
     <li className="pedido">
@@ -27,7 +48,11 @@ function DetallesPedido({ pedido }) {
         <p className="total">Total: $ {pedido.total} </p>
       </div>
       <div className="acciones">
-        <button type="button" className="btn btn-rojo btn-eliminar">
+        <button
+          onClick={() => eliminarPedido(pedido._id)}
+          type="button"
+          className="btn btn-rojo btn-eliminar"
+        >
           <i className="fas fa-times"></i>
           Eliminar Pedido
         </button>
--- cliente-api/src/components/pedidos/Pedidos.js
+++ cliente-api/src/components/pedidos/Pedidos.js
@@ -12,7 +12,7 @@ function Pedidos() {
       guardarPedidos(resultado.data);
     };
     consultarAPI();
-  }, []);
+  }, [pedidos]);
 
   return (
     <>
