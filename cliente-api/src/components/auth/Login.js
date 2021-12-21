459e6754a4001d72d1607398766251b396420736
Diego <drbv27@gmail.com>
2021-12-21 17:29:12 -0500

    sending the query to login

--- cliente-api/src/components/auth/Login.js
+++ cliente-api/src/components/auth/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import clienteAxios from "../../config/axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const leerDatos = () => {};
+  let navigate = useNavigate();
+  //state con los datos del formulario
+  const [credenciales, guardarCredenciales] = useState({});
+
+  //iniciar sesión en el servidor
+  const iniciarSesion = async (e) => {
+    e.preventDefault();
+
+    //autenticar usuario
+    try {
+      const respuesta = await clienteAxios.post(
+        "/iniciar-sesion",
+        credenciales
+      );
+      //extraer el token y colocarlo en local storage
+      const { token } = respuesta.data;
+      localStorage.setItem("token", token);
+      //alerta
+      Swal.fire("Login Correcto", "Has iniciado sesión", "success");
+      //redireccionar
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        icon: "error",
+        title: "Hubo un error",
+        text: error.response.data.mensaje,
+      });
+    }
+  };
+
+  //almacenar lo que el usuarios escribe en el state
+  const leerDatos = (e) => {
+    guardarCredenciales({
+      ...credenciales,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className="login">
       <h2>Iniciar Sesión</h2>
       <div className="contenedor-formulario">
-        <form>
+        <form onSubmit={iniciarSesion}>
           <div className="campo">
             <label>Email</label>
             <input
@@ -21,7 +61,7 @@ function Login() {
             <label>Password</label>
             <input
               type="password"
-              name="email"
+              name="password"
               placeholder="Password para iniciar sesión"
               required
               onChange={leerDatos}
