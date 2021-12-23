ff3fca60824fc877202c38bd1e1dc78119ee8c70
Diego <drbv27@gmail.com>
2021-12-23 16:42:44 -0500

    securing a component

--- cliente-api/src/components/clientes/Clientes.js
+++ cliente-api/src/components/clientes/Clientes.js
@@ -9,8 +9,10 @@ import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 //importar el context
 import { CRMContext } from "../../context/CRMContext";
+import { useNavigate } from "react-router-dom";
 
 function Clientes() {
+  let navigate = useNavigate();
   //Trabajar con el state
   //clientes es igual al state, guardarClientes es igual ala funcionpara guardar el state
 
@@ -18,17 +20,39 @@ function Clientes() {
 
   //utilizar valores del context
   const [auth, guardarAuth] = useContext(CRMContext);
-  console.log(auth);
+  /*  console.log(auth); */
 
   useEffect(() => {
-    //qUERIE A LA api
-    const consultarAPI = async () => {
-      const clientesConsulta = await clienteAxios.get("/clientes");
-      //colocar el resultado en el state
-      guardarClientes(clientesConsulta.data);
-    };
-    consultarAPI();
+    if (auth.token !== "") {
+      //qUERIE A LA api
+      const consultarAPI = async () => {
+        try {
+          const clientesConsulta = await clienteAxios.get("/clientes", {
+            headers: {
+              Authorization: `Bearer ${auth.token}`,
+            },
+          });
+          //COLOCAR EL RESULTADO EN EL STATE
+          guardarClientes(clientesConsulta.data);
+        } catch (error) {
+          //Error con autorizacion
+          if (error.response.status === 500) {
+            //Redireccionar
+            navigate("/iniciar-sesion", { replace: true });
+          }
+        }
+      };
+      consultarAPI();
+    } else {
+      //Redireccionar
+      navigate("/iniciar-sesion", { replace: true });
+    }
   }, [clientes]);
+
+  //si el state esta como false
+  if (!auth.auth) {
+    navigate("/iniciar-sesion", { replace: true });
+  }
 
   if (!clientes.length) return <Spinner />;
   return (
